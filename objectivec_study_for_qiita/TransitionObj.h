095ea36d81ed063016a78aba72f6703395061d96
kato yuki <yukihigasi@gmail.com>
2017-05-03 21:38:13 +0900

    コミット漏れ

---
+++ objectivec_study_for_qiita/TransitionObj.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+@interface TransitionObj : NSObject
+@property (nonatomic) NSString *l1;
+@property (nonatomic) NSString *l2;
+@property (nonatomic) NSString *l3;
+
+@end
